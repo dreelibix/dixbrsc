b0f79ce9c537b1ff7f8c0b1d3f7840626dbc3584
jinlong_zhao <jinlong.zhao@dreelibix.dev>
2025-03-11 17:19:56 +0900

    Commit initial.

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+/*
+The main function server as the entry point of this rust program.
+It prints a greeting message to the console.
+*/
+fn main() {
+    // Use println! macro to print a message to console.
+    // ! show that println is a macro.
+    println!("DreeLibIX's Denへようこそ！");
+}
